1f9e42983695964a2a85456e75c226fe5fb18b30
Daniel McGrane <dr.mcgrane@gmail.com>
2024-04-05 17:10:56 +0100

    revalidate every 30 mins

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { Logo } from "@/components/Logo";
 
 async function getData() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/runes`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/runes`, {
+    next: { revalidate: 1800 },
+  });
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
